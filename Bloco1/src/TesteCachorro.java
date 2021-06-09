da3df820342543e45bb338d8b64b18e070154cb6
Agda <agda.camilla@gmail.com>
2021-06-09 19:26:20 -0300

    Mudança na mensagem

--- Bloco1/src/TesteCachorro.java
+++ Bloco1/src/TesteCachorro.java
@@ -5,7 +5,7 @@ public class TesteCachorro {
 		Scanner ler = new Scanner(System.in);
 		
 		String nome;
-		System.out.println("Digite o nome do chachorro: ");
+		System.out.println("Digite o nome do bicho: ");
 		nome = ler.next();
 		
 		Cachorro gui = new Cachorro(nome, 7, 18, "médio", "macho", "sim");
