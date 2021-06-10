42e47fa899d7ee1f1f35b2bf1499211b2188c186
hanely <hanely.menezes@gmail.com>
2021-06-10 18:49:32 -0300

    hanna

--- Bloco1/src/TesteCachorro.java
+++ Bloco1/src/TesteCachorro.java
@@ -4,11 +4,11 @@ public class TesteCachorro {
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
 		
-		//Implementar escolha de cachorro ou gato usando metodo
+		//Implementar escolha de cachorro ou gato usando metodo ALGUMA COISA
 		//
 		
 		String nome;
-		System.out.println("Digite o nome: ");
+		System.out.println("Digite o nome: HANNA");
 		nome = ler.next();
 		
 		Cachorro gui = new Cachorro(nome, 7, 18, "médio", "macho", "sim");
